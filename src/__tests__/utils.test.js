f8c3586d594813716089f5a274db785efe055467
Riko-04 <echoge11@gmail.com>
2024-05-02 00:43:13 +0300

    Completes the lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -3,7 +3,7 @@ import { pointsForWord } from "../utils";
 
 describe("pointsForWord", () => {
     it("Calculates the total points for a word(1 per vowel, 2 per consonant)", () => {
-        const word = "test";
+        const word = "tEsT";
 
         const points = pointsForWord(word);
 
